02983137b193cbb4d19ba651581db36cf91f3d0e
gabriel-zvarum <38226589+gabriel-zvarum@users.noreply.github.com>
2018-05-08 10:31:41 -0300

    Classe matricula
    Criação da clase Matricula.Gets Sets e Cosntrutor

--- src/projetoescola/model/matricula.java
+++ src/projetoescola/model/matricula.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetoescola.model;
 
-/**
- *
- * @author Aluno
- */
+import java.util.Date;
+
+
 public class matricula {
+    
+    private long codigo;
+    private Date dataDaMatricula;
+
+    public matricula(long codigo, Date dataDaMatricula) {
+        this.codigo = codigo;
+        this.dataDaMatricula = dataDaMatricula;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getDataDaMatricula() {
+        return dataDaMatricula;
+    }
+
+    public void setDataDaMatricula(Date dataDaMatricula) {
+        this.dataDaMatricula = dataDaMatricula;
+    }
+
+    @Override
+    public String toString() {
+        return "matricula{" + "codigo=" + codigo + ", dataDaMatricula=" + dataDaMatricula + '}';
+    }
+    
+    
     
 }
