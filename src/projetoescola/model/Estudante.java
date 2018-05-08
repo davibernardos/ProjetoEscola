617d7b1099746822861a8f95ed83d1dd1b469868
Davi Josoé Wollmann Souza <davi.wollmann.s@gmail.com>
2018-05-08 10:04:43 -0300

    classe Estudante
    variaveis de instancia da classe estudante, metodos get, set, e contrutor;

---
+++ src/projetoescola/model/Estudante.java
@@ -0,0 +1,76 @@
+
+package projetoescola.model;
+
+ import java.util.Date;
+
+public class Estudante {
+
+    public Estudante() {
+    }
+
+    public Estudante(long codigo, int matricula, Date dataDaMatricula, String nomeDoPai, String nomeDaMae, String estadoCivil) {
+        this.codigo = codigo;
+        this.matricula = matricula;
+        this.dataDaMatricula = dataDaMatricula;
+        this.nomeDoPai = nomeDoPai;
+        this.nomeDaMae = nomeDaMae;
+        this.estadoCivil = estadoCivil;
+    }
+    
+    private long codigo;
+    private int matricula;
+    private Date dataDaMatricula;
+    private String nomeDoPai;
+    private String nomeDaMae;
+    private String estadoCivil;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getDataDaMatricula() {
+        return dataDaMatricula;
+    }
+
+    public void setDataDaMatricula(Date dataDaMatricula) {
+        this.dataDaMatricula = dataDaMatricula;
+    }
+
+    public String getNomeDoPai() {
+        return nomeDoPai;
+    }
+
+    public void setNomeDoPai(String nomeDoPai) {
+        this.nomeDoPai = nomeDoPai;
+    }
+
+    public String getNomeDaMae() {
+        return nomeDaMae;
+    }
+
+    public void setNomeDaMae(String nomeDaMae) {
+        this.nomeDaMae = nomeDaMae;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    
+    
+}
