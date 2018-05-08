4101159526bfe0a0d69e7f8d80ba62c817856696
Davi Josoé Wollmann Souza <davi.wollmann.s@gmail.com>
2018-05-08 10:46:14 -0300

    Pacote controller e view
    alteração dos locais das classes

--- src/projetoescola/controller/FXMLDocumentController.java
+++ src/projetoescola/controller/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projetoescola;
+package projetoescola.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
