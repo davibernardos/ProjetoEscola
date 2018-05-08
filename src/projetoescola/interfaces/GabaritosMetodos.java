0c3078b2510ea9146a4fb41b2a3207196df042ae
gabriel-zvarum <38226589+gabriel-zvarum@users.noreply.github.com>
2018-05-08 11:08:27 -0300

    Interface

---
+++ src/projetoescola/interfaces/GabaritosMetodos.java
@@ -0,0 +1,13 @@
+package projetoescola.interfaces;
+
+public interface GabaritosMetodos {
+   
+    public void testaCompilacao();
+    public void variaveisEsperadas();
+    public void funcoesEsperadas();
+    public void lacosEsperados();
+    public void tempoDeExecucao();
+    public void complexidadeDoAlgoritmo();
+    public void resultadosEsperados();
+    
+}
