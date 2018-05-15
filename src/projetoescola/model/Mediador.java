a272a3c37a62f14da60ab0d1816c7e0beea9e105
davibernardos <davibernardos@gmail.com>
2018-05-15 10:33:03 -0300

    Classes JPA

--- src/projetoescola/model/Mediador.java
+++ src/projetoescola/model/Mediador.java
@@ -5,8 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) 
 public class Mediador extends Pessoa implements Serializable {
     
     private String titulacao;
