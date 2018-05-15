469772fe4733b0c14fcc3eab41ef95dc6b297976
davibernardos <davibernardos@gmail.com>
2018-05-15 10:56:07 -0300

    Mudanças persistence

--- src/projetoescola/model/Mediador.java
+++ src/projetoescola/model/Mediador.java
@@ -2,9 +2,6 @@ package projetoescola.model;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
